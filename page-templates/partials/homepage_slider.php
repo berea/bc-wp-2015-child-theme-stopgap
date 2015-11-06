b98af54474d3cdc4ec60a81072257ed447d09c79
Janine Hempy <jhempy@gmail.com>
2015-11-06 09:51:54 -0500

    Changes class on homepage slider to correct placement issue.

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -1,5 +1,5 @@
 <!-- Stopgap homepage slider -->
-<div class="homepage-slider homepage-slider-sizing-stopgap homepage-slider-sizing-aspect-ratio ratio_1024x192 ">
+<div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192 ">
     <div class="secondary-slider-background"></div>
     <div class="slider-textblock">
         <div class="slider-breadcrumbs"><?php if ( function_exists('yoast_breadcrumb') ) {
